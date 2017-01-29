22a64f256666ed18664256625c24afdab017d9cb
Nicolas Vargas <nvortega92@gmail.com>
2017-01-29 21:52:49 +0100

    Fixed some bugs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,22 @@ package controllers
 import play.api.mvc.{Action, Controller}
 
 
-object Application extends Controller {
+class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  def services = Action {
+    Ok(views.html.services())
+  }
+
+  def about = Action {
+    Ok(views.html.about())
+  }
+
+  def contact = Action {
+    Ok(views.html.contact())
   }
 
 }
