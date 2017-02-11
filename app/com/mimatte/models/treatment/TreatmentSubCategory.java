8f830cc36067a7fafdc6a3e93ea71abce11cfc89
Nicolas Vargas <nvortega92@gmail.com>
2017-02-11 15:08:49 +0100

    Added getters/setters to TreatmentSubCategory model

--- app/com/mimatte/models/treatment/TreatmentSubCategory.java
+++ app/com/mimatte/models/treatment/TreatmentSubCategory.java
@@ -2,6 +2,7 @@ package com.mimatte.models.treatment;
 
 import com.mimatte.models.BaseModel;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class TreatmentSubCategory extends BaseModel {
@@ -11,4 +12,72 @@ public class TreatmentSubCategory extends BaseModel {
     private TreatmentCategory treatmentCategory;
 
     private List<Treatment> treatments;
+
+    public TreatmentSubCategory() {}
+
+    public TreatmentSubCategory(
+            Long id,
+            LocalDateTime deletedAt,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt,
+            String code,
+            String name,
+            TreatmentCategory treatmentCategory
+    ) {
+        super(id, deletedAt, createdAt, updatedAt);
+
+        this.code = code;
+        this.name = name;
+        this.treatmentCategory = treatmentCategory;
+    }
+
+    public TreatmentSubCategory(
+            Long id,
+            LocalDateTime deletedAt,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt,
+            String code,
+            String name,
+            TreatmentCategory treatmentCategory,
+            List<Treatment> treatments
+    ) {
+        super(id, deletedAt, createdAt, updatedAt);
+
+        this.code = code;
+        this.name = name;
+        this.treatmentCategory = treatmentCategory;
+        this.treatments = treatments;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TreatmentCategory getTreatmentCategory() {
+        return treatmentCategory;
+    }
+
+    public void setTreatmentCategory(TreatmentCategory treatmentCategory) {
+        this.treatmentCategory = treatmentCategory;
+    }
+
+    public List<Treatment> getTreatments() {
+        return treatments;
+    }
+
+    public void setTreatments(List<Treatment> treatments) {
+        this.treatments = treatments;
+    }
 }
