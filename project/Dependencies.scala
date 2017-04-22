bb397f61b7ef8d785fc84713b5744c26abdbf40e
Nicolas Vargas <nvortega92@gmail.com>
2017-04-22 19:37:15 +0200

    Updated Mysql j connector version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val guice = "com.google.inject" % "guice" % "4.1.0"
-  val mysql = "mysql" % "mysql-connector-java" % "5.1.40"
+  val mysql = "mysql" % "mysql-connector-java" % "5.1.41"
   val typeSafeConfig = "com.typesafe" % "config" % "1.3.1"
 
   val backendDeps = Seq(guice, mysql, typeSafeConfig)
